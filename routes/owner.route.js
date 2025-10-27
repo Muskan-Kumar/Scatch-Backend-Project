e465304125ac2ff5e04101ceeced7436e60d3a75
Muskan Kumar <muskankumar952593@gmail.com>
2025-10-27 16:53:52 +0530

    Owner route created

--- routes/owner.route.js
+++ routes/owner.route.js
@@ -1,6 +1,26 @@
 import express from "express"
+import { Owner } from "../models/owner.model.js"
 
 const router = express.Router()
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async (req, res)=>{
+        let owner = await Owner.find();
+        if(owner.length > 0){
+            return res.status(503).send("You don't permission to create a new owner.")
+        }
+        const {fullname, email, password} = req.body;
+
+        const createdOwner = await Owner.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner)
+    })
+}
+
+
 
 // routes
 router.get("/", (req,res)=>{
